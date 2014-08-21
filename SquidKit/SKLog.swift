f019316a58d0b83c32abee9c0ff4a36546542f1a
Mike Leavy <mleavy@gmail.com>
2014-08-21 10:12:19 -0700

    use autoclosure for logMessage so that parameters aren't evalutated if logging is disabled

--- SquidKit/SKLog.swift
+++ SquidKit/SKLog.swift
@@ -7,23 +7,31 @@
 //
 
 import Foundation
+import UIKit
 
 public class SKLog {
-    public class func logMessage(message:String?) {
-#if arch(i386)
-    if (message != nil) {
-        NSLog(message!)
-    }
-#endif
+    
+    public class func logMessage(output: @autoclosure() -> String?) {
+        #if arch(i386) || arch(x86_64)
+            if let message = output() {
+                NSLog(message)
+            }
+        #endif
     }
     
     public class func logSimulatorAppBundleURL() {
-#if arch(i386)
-        SKLog.logMessage("Simulator app bundle URL: \(NSBundle.mainBundle().bundleURL)")
-#endif
+        #if arch(i386) || arch(x86_64)
+            SKLog.logMessage("Simulator app bundle URL: \(NSBundle.mainBundle().bundleURL)")
+        #endif
     }
     
     public class func logRect(rect:CGRect, message:String = "") {
         SKLog.logMessage(message + "rect -> x: \(rect.origin.x); y: \(rect.origin.y); width: \(rect.size.width); height: \(rect.size.height)");
+    }
+    
+    public class func autoLog(output: @autoclosure() -> String) {
+        #if arch(i386)
+            output()
+        #endif
     }
 }
