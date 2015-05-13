cd63d99f4906c4d941935c5c15703321911f514c
Mike Leavy <mleavy@gmail.com>
2015-05-12 22:52:52 -0700

    add UIDevice+Utility (and method to check if simulator)

---
+++ SquidKit/UIDevice+Utility.swift
@@ -0,0 +1,21 @@
+//
+//  UIDevice+Utility.swift
+//  SquidKit
+//
+//  Created by Mike Leavy on 5/12/15.
+//  Copyright (c) 2015 SquidKit. All rights reserved.
+//
+
+import UIKit
+
+public extension UIDevice {
+    public func simulator() -> Bool {
+        var isSimulator = false
+        
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+        isSimulator = true
+        #endif
+        
+        return isSimulator
+    }
+}
