a63379e5fd6cf94a567cc0a096c5d744caaa2f3f
Mike Leavy <mleavy@gmail.com>
2016-03-31 14:30:00 -0700

    add methods to access/read files in Documents directory

--- SquidKit/String+File.swift
+++ SquidKit/String+File.swift
@@ -30,6 +30,26 @@ public extension String {
         return nil
     }
     
+    public static func stringWithPathToDocumentsFile(fileName:String) -> String? {
+        if let path = String.pathToDocumentsDirectory() {
+            var url = NSURL(fileURLWithPath: path)
+            url = url.URLByAppendingPathComponent(fileName)
+            return url.path
+        }
+        
+        return nil
+    }
+    
+    public static func stringWithContentsOfDocumentsFile(fileName:String) -> String? {
+        if let filePath = String.stringWithPathToDocumentsFile(fileName) {
+            if let data = NSFileManager.defaultManager().contentsAtPath(filePath) {
+                return NSString(data:data, encoding:NSUTF8StringEncoding) as? String
+            }
+        }
+        
+        return nil
+    }
+    
     public static func pathToApplicationBundle() -> String {
         return NSBundle.mainBundle().bundlePath
     }
