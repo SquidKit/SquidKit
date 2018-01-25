efe38382266ee260221bc12659560e9d859c3f7b
Mike Leavy <mleavy@gmail.com>
2018-01-24 21:50:09 -0800

    add preserveBackgroundColors UIView class method

--- SquidKit/UIView+Utility.swift
+++ SquidKit/UIView+Utility.swift
@@ -10,6 +10,16 @@ import UIKit
 
 public extension UIView {
     
+    public class func preserveBackgroundColors(views: [UIView], closure: () -> Void) {
+        let viewColorPairs = views.map { (view) -> (UIView, UIColor?) in
+            return (view, view.backgroundColor)
+        }
+        closure()
+        for (view, color) in viewColorPairs {
+            view.backgroundColor = color
+        }
+    }
+    
     public func centerInView(_ containingView:UIView) {
         var rect = self.frame
         rect.origin.x = floor(containingView.bounds.origin.x + (containingView.bounds.size.width - rect.size.width) / 2)
@@ -18,3 +28,5 @@ public extension UIView {
         self.frame = rect
     }
 }
+
+
